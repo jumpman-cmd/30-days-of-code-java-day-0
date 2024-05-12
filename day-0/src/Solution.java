57e348111b15f49869072dcec6b0ffd964d78802
jumpman-cmd <codeks7407@gmail.com>
2024-05-12 19:57:02 +0200

    Initial commit

---
+++ day-0/src/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution
+{
+
+    public static void main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        // Create a Scanner Object.
+        Scanner myObject = new Scanner(System.in);
+
+        // Read user input.
+        String newObject = myObject.nextLine();
+        System.out.println("Hello, World.\n" + newObject);
+    }
+}
